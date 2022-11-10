40375e2d6976cc991b47ed6ef15704288cf4da0e
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2022-11-10 19:12:56 +0530

    Add controller to routers

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+// use express router
+// require('./routes/index) is similar to require('./routes) in this case, it by default fetch routes
+app.use('/',require('./routes/index'))
+
 
 app.listen(port, (err) => {
     if (err) {
