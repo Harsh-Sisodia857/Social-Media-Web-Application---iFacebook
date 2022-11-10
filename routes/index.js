f4fa32e363f5a7e42e7632c60ddf20b744754a08
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2022-11-10 19:27:08 +0530

    Make Post Route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const homeController = require('../controllers/home_controller');
 
 const router = express.Router();
 
-router.get('/',homeController.home)
+router.get('/', homeController.home)
 
+// Whenever the pattern of route is /users handle it in users file
+router.use('/users',require('./users'))
 
 module.exports = router;
