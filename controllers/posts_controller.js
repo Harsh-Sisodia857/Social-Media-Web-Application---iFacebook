fb1e3e2424f3cebcd723c09f47e43497ccad77f0
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2022-11-14 18:11:16 +0530

    Deleting Post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post')
+const Comment = require('../models/comment')
+
 
 module.exports.createPost = function (req, res) {
     console.log("Create Post is Called :");
@@ -13,5 +15,21 @@ module.exports.createPost = function (req, res) {
         }
         console.log(post);
         return res.redirect('back');
+    })
+}
+
+module.exports.destroy = function (req, res) {
+    Post.findById(req.params.id, function (err, post) {
+        console.log("req.user : ", req.user);
+        // we compare id using .id instead of _id, facility provided by mongodb because of String matching req.user is String and req.user.id is also String
+        if (post.user == req.user.id) {
+            post.remove();
+            Comment.deleteMany({ post: req.params.id }, function (err) {
+                return res.redirect('back');
+            })
+        }
+        else {
+            return res.redirect('back');
+        }
     })
 }
