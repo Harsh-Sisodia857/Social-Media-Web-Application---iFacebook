1186aaa46dcecea5237966b4dcd7f3bc0f851724
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2022-11-14 16:57:21 +0530

    Showing the comments on the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,34 @@
 const Post = require('../models/post')
 
 module.exports.home = function (req, res) {
-
-    Post.find({}).populate('user').exec(function (err, posts) {
+    // model of post --------------------------------------------------------------------------
+    // const postSchema = new mongoose.Schema({
+    //     content: {
+    //         type: String,
+    //         required: true
+    //     },
+    //     user: {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: 'User'
+    //     },
+    //     // include the array of ids of all comments in this post schema
+    //     comments: [
+    //         {
+    //             type: mongoose.Schema.Types.ObjectId,
+    //             ref: 'Comment'
+    //         }
+    //     ]
+    // }, {
+    //     timestamps: true
+    // })
+    // -------------------------------------------------------------------------
+    // from post first we populate the user(fetch the user detail using its id) then we populate the comments and also show showing  the user's name who comment for that we also populate user from the comment
+    Post.find({}).populate('user').populate({
+        path: 'comments',
+        populate: {
+            path : 'user'
+        }
+    }).exec(function (err, posts) {
         return res.render('home', {
             title: "Home",
             posts
