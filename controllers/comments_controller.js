d6c3e46bead9e6d445a38d583685b0374438ed5f
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2022-11-14 18:23:23 +0530

    Deleting Comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -21,3 +21,19 @@ module.exports.createComment = function (req, res) {
         }
     })
 }
+
+module.exports.destroy = function (req, res) {
+    Comment.findById(req.params.id, function (err, comment) {
+        if(comment.user = req.user.id)
+        {
+            let PostId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(PostId, { $pull : {comments : req.params.id}} ,function (err, post) {
+                return res.redirect('back');
+            })
+        }
+        else {
+            return res.redirect('back');
+        }
+    })
+}
